19054d752b5c2fbe7e7277e15d43d0f1dce9bc56
Sarah Carlisle <sarah@travelescaperoom.com>
2020-10-17 19:15:27 -0500

    finished server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
-const favoriteRouter = require("./routes/favorite.router");
-const categoryRouter = require("./routes/category.router");
-const searchRouter = require("./routes/display.router");
+// available routes
+const displayRouter = require("./routes/display.router");
+const editRouter = require("./routes/edit.router");
+const genresRouter = require("./routes/genres.router");
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -13,7 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use("/api/display", displayRouter);
+app.use("/api/edit", editRouter);
+app.use("/api/genres", genresRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
